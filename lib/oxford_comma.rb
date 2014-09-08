9596252d8246b433e4eac101c6d61acf16b4bbfa
Katie Hoffman <ktahoffman@gmail.com>
2014-09-08 13:00:00 -0400

    add passing code

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")    
 end
