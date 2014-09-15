8c5fc6aee1c80ae08000534f51671d8f5cd824c0
Katie Hoffman <ktahoffman@gmail.com>
2014-09-15 14:24:19 -0400

    requiring spec helper twice is redundant

--- spec/oxford_comma_spec.rb
+++ spec/oxford_comma_spec.rb
@@ -1,5 +1,3 @@
-require_relative 'spec_helper'
-
 describe "#oxford_comma" do
   it 'returns a string without any additional fomatting when given a 1-element array' do
     expect(oxford_comma(["kiwi"])).to eq("kiwi")
