f875a5f12b4bf2fb4d7047471893f21e7a999cff
ihollander <ianholla@gmail.com>
2021-07-09 12:13:59 -0400

    Updated solution

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
-  end
-  array.join(", ")    
+  return array.join(" and ") if array.size < 3
+
+  # insert 'and ' in front of the last string in the array
+  array[-1] = "and #{array[-1]}"
+
+  array.join(", ")  
 end
